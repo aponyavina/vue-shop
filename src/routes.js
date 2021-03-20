330dfeaf37442729074a82da98f82b8016483341
aponyavina <ponyavina1997@mail.ru>
2021-03-20 16:33:49 +0300

    vue-shop init

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// default
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+// pages
+import Home from '@/pages/Home'
+import Shop from '@/pages/Shop'
+import NotFound from '@/pages/404'
+
+// routering
+export default new Router ({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/shop',
+            name: 'shop',
+            component: Shop
+        },
+        {
+            path: '*',
+            name: 'NotFound',
+            component: NotFound
+        }
+    ]
+})
